88ff3f0f0f93695dddb4bb345f266d1fa7fc47a1
Kai Sassnowski <ksassnowski@gmail.com>
2018-05-19 16:58:11 +0200

    Upgrade formula to version 0.5.1

--- kimai-cli.rb
+++ kimai-cli.rb
@@ -3,8 +3,8 @@ class KimaiCli < Formula
 
   desc "CLI for Kimai time tracking software"
   homepage "https://github.com/ksassnowski/kimai-cli"
-  url "https://github.com/ksassnowski/kimai-cli/releases/download/0.5.0/kimai-cli-0.5.0.tar.gz"
-  sha256 "5590f7cc7533ca992c791a3db62be5334ab0fd783de2530c3f71e47902c21019"
+  url "https://github.com/ksassnowski/kimai-cli/releases/download/0.5.1/kimai-cli-0.5.1.tar.gz"
+  sha256 "0ff6961c02c0cc3e4ebec67e71378c37a754a382b6e074c8a3e4d8d64b9899d0"
 
   depends_on "python"
 
